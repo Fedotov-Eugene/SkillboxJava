51c6a87d761120f719a01a92a3ab168c9cf03d9d
MrViolent <you@example.com>
2019-11-14 20:51:48 +0300

    $ add homework 5

---
+++ module4/5/StringExperiments/src/Loader.java
@@ -0,0 +1,18 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String numbers = text.replaceAll("[^0-9]", " ");
+        String [] array = numbers.replaceAll("\\s+", " ").trim().split(" ");
+
+        int number1 = Integer.parseInt(array[0]);
+        int number2 = Integer.parseInt(array[1]);
+        int number3 = Integer.parseInt(array[2]);
+
+        int count = number1 + number2 + number3;
+
+        System.out.println(count);
+    }
+}
