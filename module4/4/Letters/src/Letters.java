6a793a16fa514466360c7b9a997e15f021a1b577
MrViolent <you@example.com>
2019-11-20 02:15:29 +0300

    fixed Letters project

--- module4/4/Letters/src/Letters.java
+++ module4/4/Letters/src/Letters.java
@@ -1,10 +1,10 @@
 public class Letters {
     public static void main(String[] args) {
         String alphabet = "abcdefgehijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        //System.out.println(alphabet.length());
+
         for (int i = 0; i < alphabet.length(); i++){
             char ch = alphabet.charAt(i);
-            System.out.println("Код буквы " + ch + ": " + (int) ch);
+            System.out.printf("'%c': %4d %n", ch, (int) ch);
         }
     }
 }
