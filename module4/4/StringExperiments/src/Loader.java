af8c0be9898615b5525423a865536618b42d1cc2
MrViolent <you@example.com>
2019-11-20 01:11:02 +0300

    fixed StringExperiments project

--- module4/4/StringExperiments/src/Loader.java
+++ module4/4/StringExperiments/src/Loader.java
@@ -3,15 +3,17 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String numbers = text.replaceAll("[^0-9]", " ");
-        String [] array = numbers.replaceAll("\\s+", " ").trim().split(" ");
+        String text = "Саша заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
-        int number1 = Integer.parseInt(array[0]);
-        int number3 = Integer.parseInt(array[2]);
+        int index1 = text.indexOf(' ', 14);
+        int index2 = text.lastIndexOf(' ', 20);
+        int index3 = text.indexOf(' ', 55);
+        int index4 = text.lastIndexOf(' ', 62);
 
-        int count = number1 + number3;
+        int sasha = Integer.parseInt(text.substring(index1, index2).trim());
+        int masha = Integer.parseInt(text.substring(index3, index4).trim());
 
-        System.out.println(count);
+        int sum = sasha + masha;
+        System.out.println(sum);
     }
 }
