2ae9b1c378e4d1adea75a5f17d508f5742193573
MrViolent <you@example.com>
2019-11-22 01:21:46 +0300

    сreated project FullName

---
+++ module4/5/FullName/src/FullName.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FullName {
+    public static void main(String[] args) {
+        System.out.print("Введите ФИО: ");
+        Scanner fullName = new Scanner(System.in);
+        String text = fullName.nextLine();
+
+        String[] array = text.trim().split("\\s+");
+
+        if (array.length == 3) {
+            System.out.println("Фамилия: " + array[0]
+                    + "\nИмя: " + array[1]
+                    + "\nОтчество: " + array[2]);
+        } else {
+            System.out.println("Вы ввели неполные данные!!!");
+        }
+    }
+}
